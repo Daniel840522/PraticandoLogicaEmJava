579756b12b66e6c11ee1f208e1af89639c834bfa
Daniel Monteiro <danielchsan@gmail.com>
2023-04-09 14:11:39 -0300

    new Commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,58 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+
+import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        String name = "Sara Connor";
+        String accountType = "Checking Account";
+        double balance = 2500.0;
+        int choice = 0;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("********************************");
+        System.out.println("Customer name is: " + name);
+        System.out.println("Account type is: " + accountType);
+        System.out.println("The account balance is: " + balance);
+        System.out.println("********************************");
+
+        String menu = """
+                Menu
+                __________________
+                1 - Check balance
+                2 - Cash deposit
+                3 - Withdraw money
+                4 - Exit
+                __________________
+                Choose your option:
+                """;
+        Scanner option = new Scanner(System.in);
+
+        while (choice != 4) {
+
+            System.out.println(menu);
+            choice = option.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Your balance is: " + balance);
+                    break;
+                case 2:
+                    Scanner cash = new Scanner(System.in);
+                    System.out.println("Enter the value: ");
+                    double newValue = cash.nextDouble();
+                    balance += newValue;
+                    break;
+                case 3:
+                    Scanner money = new Scanner(System.in);
+                    System.out.println("Enter the value: ");
+                    double lostMoney = money.nextDouble();
+                    balance -= lostMoney;
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Choose a correct option");
+                    break;
+            }
         }
     }
 }
